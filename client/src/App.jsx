946213480db6ae0643ee76901f6a4be89150c853
Vaddyn Codes <goswamihardik@outlook.com>
2026-06-10 18:02:19 +0530

    add the new routes and dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Home from "./pages/home";
+import login from "./pages/login";
 import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <>
-    <Home />
-    </>
+    <browserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<login />} />
+        </Routes>
+      </Router>
+    </browserRouter>
   );
 }
 
